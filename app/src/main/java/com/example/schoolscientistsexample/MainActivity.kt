1cbad788685ad5e9138e1e3aa7b38d8c197746d1
Aleksey Volkov <alekseyvolk@yandex.ru>
2020-07-05 14:08:24 +0700

    [+] WebRequest ledOn, ledOff

--- app/src/main/java/com/example/schoolscientistsexample/MainActivity.kt
+++ app/src/main/java/com/example/schoolscientistsexample/MainActivity.kt
@@ -3,12 +3,17 @@ package com.example.schoolscientistsexample
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 import java.lang.NumberFormatException
 
+
 class MainActivity : AppCompatActivity() {
+
+
+    var t = ServerCommand()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -41,7 +46,7 @@ class MainActivity : AppCompatActivity() {
         textView.text = count.toString();
     }
 
-    fun randomMe (view: View) {
+    fun randomMe(view: View) {
 
         // Create an Intent to start the second activity
         val randomIntent = Intent(this, SecondActivity::class.java)
@@ -64,5 +69,13 @@ class MainActivity : AppCompatActivity() {
 
         // Start the new activity.
         startActivity(randomIntent)
+    }
+
+    fun ledOn(view: View){
+        t.ledOn()
+    }
+
+    fun ledOff(view: View){
+        t.ledoff()
     }
 }
