5791440801cb932cef6ebb5e7be22bd2db3e1993
Aleksey Volkov <alekseyvolk@yandex.ru>
2020-07-15 21:20:16 +0700

    [+] добавил "сырой" отыет от сервера на форму для приложения Михаила И

--- app/src/main/java/com/example/schoolscientistsexample/MichaelIMainActivity.kt
+++ app/src/main/java/com/example/schoolscientistsexample/MichaelIMainActivity.kt
@@ -2,11 +2,21 @@ package com.example.schoolscientistsexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_michael_i_main.*
+import kotlinx.android.synthetic.main.activity_michael_i_main.view.*
 
 class MichaelIMainActivity : AppCompatActivity() {
+
+    private var t = ServerCommandMichaelI()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_michael_i_main)
+    }
+
+    fun scopeGet(view: View){
+        var str = t.gameScoreGet()
+        textViewTeam2Scope.text = str
     }
 }
