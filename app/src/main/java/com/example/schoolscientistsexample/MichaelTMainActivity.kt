b4d13bc52046f73902dbe258c1602a6c4c775cee
Aleksey Volkov <alekseyvolk@yandex.ru>
2020-07-15 17:30:46 +0700

    [+] Добавил разбор JSON в модель пользователя для окна для Михаила Т

--- app/src/main/java/com/example/schoolscientistsexample/MichaelTMainActivity.kt
+++ app/src/main/java/com/example/schoolscientistsexample/MichaelTMainActivity.kt
@@ -3,6 +3,9 @@ package com.example.schoolscientistsexample
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import com.google.gson.Gson
+import com.google.gson.JsonParser
+import com.google.gson.reflect.TypeToken
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_michael_t_main.*
 
@@ -16,7 +19,13 @@ class MichaelTMainActivity : AppCompatActivity() {
     }
 
     fun userInfoGet(view: View){
-        val str = t.userInfoGet(editTextUserId.text.toString())
-        textViewUserId.text = str
+        val json = t.userInfoGet(editTextUserId.text.toString())
+
+        val gson = Gson()
+        val michaelTUserType = object : TypeToken<MichaelTUserInfo>() {}.type
+
+        var userInfo: MichaelTUserInfo = gson.fromJson(JsonParser().parse(json).getAsJsonObject().get("object"), michaelTUserType)
+
+        textViewUserId.text = json
     }
 }
